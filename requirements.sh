5f60c02708746cc23b43c85679ab77908e8f5d7e
RadmirZ <69034231+RadmirZ@users.noreply.github.com>
2021-08-08 17:32:34 +0300

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+pip install numpy
+pip install requests
+pip install nltk
+pip install pymorphy2
+pip install gensim
+pip install tqdm
+pip install pandas
+pip install logging
+pip install contextlib
+
